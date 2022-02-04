49707afde970ad8116d0193e061e0737f0313d25
tommysor <tommysor@gmail.com>
2022-02-04 23:45:29 +0100

    anonymous login

--- FluentFTP.Tests.System/UnitTest1.cs
+++ FluentFTP.Tests.System/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace FluentFTP.Tests.System
         [Fact]
         public void Test1()
         {
-			using (var client = new FtpClient("localhost"))
+			using (var client = new FtpClient("localhost", 21, "anonymous", "anonymous@domain.com"))
 			{
 				client.Connect();
 				var list = client.GetListing();
