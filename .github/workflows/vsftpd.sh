a3b181353a6306f6dfbd549c2e6846949e6f8fb6
tommysor <tommysor@gmail.com>
2022-02-06 15:25:49 +0100

    t

--- .github/workflows/vsftpd.sh
+++ .github/workflows/vsftpd.sh
@@ -1,5 +1,5 @@
-          docker run -d -v $ftpDir:/home/vsftpd \
-          -p 20:20 -p 21:21 -p 21100-21110:21100-21110 \
-          -e FTP_USER=testUser -e FTP_PASS=testPass \
-          -e PASV_ADDRESS=127.0.0.1 -e PASV_MIN_PORT=21100 -e PASV_MAX_PORT=21110 \
-          --name vsftpd --restart=always fauria/vsftpd
+docker run -d -v $ftpDir:/home/vsftpd \
+	-p 20:20 -p 21:21 -p 21100-21110:21100-21110 \
+	-e FTP_USER=testUser -e FTP_PASS=testPass \
+	-e PASV_ADDRESS=127.0.0.1 -e PASV_MIN_PORT=21100 -e PASV_MAX_PORT=21110 \
+	--name vsftpd --restart=always fauria/vsftpd
