fa97df181db66e76120f8efd5450e4cf89f83f98
tommysor <tommysor@gmail.com>
2022-02-06 14:39:23 +0100

    t

--- FluentFTP.Tests.Integration/FileUtil.cs
+++ FluentFTP.Tests.Integration/FileUtil.cs
@@ -10,11 +10,20 @@ namespace FluentFTP.Tests.Integration
 {
 	internal class FileUtil
 	{
+		private static readonly Random rnd = new Random();
 		internal static Stream GetSimpleTextFile()
 		{
 			var file = new MemoryStream();
 			var fileWriter = new StreamWriter(file, Encoding.UTF8);
-			fileWriter.WriteLine("abc");
+
+			var stringBuilder = new StringBuilder();
+			for (var i = 0; i < 10; i++)
+			{
+				var c = rnd.Next('a', 'z' + 1);
+				stringBuilder.Append(c);
+			}
+
+			fileWriter.WriteLine(stringBuilder.ToString());
 			file.Position = 0;
 			return file;
 		}
